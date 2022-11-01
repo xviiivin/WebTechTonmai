534e3c28182ed56cc259bc81b7e5b04b7fe6fc78
7E8J <103146285+7E8J@users.noreply.github.com>
2022-11-01 15:38:59 +0700

    update 1/11/65

---
+++ README.md
@@ -0,0 +1,5 @@
+# Warning
+
+อย่าลืม import script นะจุ๊บๆ
+
+Version 1.0.0
---
+++ src/home.js
@@ -0,0 +1,23 @@
+let reccommend_tree = gsap.timeline({
+  paused: true,
+  repeat: 0,
+});
+const recommend_tree_dropdown = reccommend_tree.to("#recommend_tree_dropdown", {
+  duration: 0.3,
+  y: 50,
+  ease: "expo-out",
+  opacity: 1,
+  display: "block",
+});
+// .to("#recommend_tree_dropdown", {
+//   duration: 0.3,
+//   opacity: 1,
+//   display: "block",
+// });
+const recommend_tree_hover = document.getElementById("recommend_tree");
+recommend_tree_hover.addEventListener("mouseenter", () =>
+  recommend_tree_dropdown.play()
+);
+recommend_tree_hover.addEventListener("mouseleave", () =>
+  recommend_tree_dropdown.reverse()
+);
