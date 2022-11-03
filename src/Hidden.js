69f43451466bc4a21483781a0a197922bd1ad722
Thanakorn Sriwannawit <64070040@kmitl.ac.th>
2022-11-03 19:17:01 +0700

    add conditional button

--- src/Hidden.js
+++ src/Hidden.js
@@ -4,10 +4,12 @@ btn.addEventListener('click', function () {
   if (x.style.display == "block") {
     Summary.innerText = "Show summary";
     x.style.display = "none";
+    chevron.className = "fa-solid fa-chevron-down ml-2"
   }
   else {
     Summary.innerText = "Hide summary";
     x.style.display = "block";
+    chevron.className = "fa-solid fa-chevron-up ml-2"
     gsap.from("#Toggle", {
       opacity: 0,
       y: -20,
